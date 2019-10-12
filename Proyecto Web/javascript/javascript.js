87ebf6ec4f5cbcc1ee4942028598c8b54b71f3b0
titoitla <56462779+titoitla@users.noreply.github.com>
2019-10-11 23:14:38 -0400

    Proyecto Web - Tito Matos

---
+++ Proyecto Web/javascript/javascript.js
@@ -0,0 +1,14 @@
+<!DOCTYPE	html> 
+<html	lang="es"> 
+<head>		
+	<meta	charset="UTF-8">		
+	<title>Mi	primer	script</title> 
+</head>
+
+<body>		
+	<script> 
+		'use strict';
+		document.writeln('Hola	Mundo!');		
+	</script> 
+</body> 
+</html>
